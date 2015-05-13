6ad28acba047943de08428501e79581c39cf1284
Evan <evan@mobilogics.com.tw>
2015-05-13 15:39:20 +0800

    project init

---
+++ MRT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MRT
+//
+//  Created by Inflames on 2015/5/13.
+//  Copyright (c) 2015年 Inflames. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
